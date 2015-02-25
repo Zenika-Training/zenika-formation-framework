53c7c9c5a81d9355616ee03211ba705ce0aa22cc
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-02-25 18:08:07 +0100

    Merge branch 'modif_nodejs' of github.com:adogor/Formation--Framework into adogor-modif_nodejs
    Conflicts:
    	Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function (grunt) {
 
             var transform = through(function (data) {
                 var out = data
-                        .replace(/!\[([\w|\s]*)][\s]*\(([\w|\s|\-|\.|\/]*)\)/g, function (match, p1, p2, src) {
+                        .replace(/!\[([\w|\s|\.]*)][\s]*\(([\w|\s|\-|\.|\/]*)\)/g, function (match, p1, p2, src) {
                             return '![' + p1 + '](' + path.resolve('CahierExercices', p2) + ')';
                         })
                         .replace(/<img (.*)src="([\w|\-|\.|\/]*)"(.*)\/?>/g, function (match, p1, p2, p3, src) {
