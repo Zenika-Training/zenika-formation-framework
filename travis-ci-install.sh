663468fdc291be0fac4925714ee6e8c979284d8f
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-08-04 17:19:18 +0200

    Travis yaml file should be in Model

---
+++ travis-ci-install.sh
@@ -0,0 +1,8 @@
+npm install;
+if [[ ! -f google-cloud-sdk/path.bash.inc ]]; then
+  echo "Downloading google cloud sdk";
+  wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip;
+  unzip -qq google-cloud-sdk.zip;
+  google-cloud-sdk/install.sh --usage-reporting false --path-update false --rc-path=~/.bashrc --bash-completion false --override-components=app;
+fi;
+google-cloud-sdk/bin/gcloud components update preview app --quiet;
