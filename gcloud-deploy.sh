13029fb746c6f3b43ee3635743084fa488506681
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-08-05 12:22:22 +0200

    add more debug

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,4 +1,5 @@
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
+echo "Deploying to FORMATION_DEPLOY_NAME"
 google-cloud-sdk/bin/gcloud --project FORMATION_DEPLOY_NAME preview app deploy --version $CURRENT_BRANCH --quiet dist/app.yaml
