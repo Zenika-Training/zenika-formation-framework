105ce83d3e4c0e33be812f22a3a8420fa09a651e
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-08-05 10:49:30 +0200

    remove name of CI tool inside the scripts

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,3 +1,3 @@
 echo $GAE_KEY_FILE_CONTENT > file.key
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
-google-cloud-sdk/bin/gcloud --project FORMATION_DEPLOY_NAME preview app deploy --version $TRAVIS_BRANCH --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project FORMATION_DEPLOY_NAME preview app deploy --version $CURRENT_BRANCH --quiet dist/app.yaml
