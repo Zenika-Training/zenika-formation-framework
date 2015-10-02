7aa0c5853d735f4e760769b1f786580f3f075ef7
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-10-02 15:58:19 +0200

    feat: compatiblité NPM2 `run.js`
    - utilisation de promesses
    - optimisation des chargements
    - configuration paramétrique via l'appel d'un fichier CSS

--- reveal/run.js
+++ reveal/run.js
@@ -1,40 +1,23 @@
-(function () {
+(function (window, document, Promise, Reveal) {
 
-  applyPrintStylesheets();
-  insertSlides(function () {
-    runReveal();
-  });
+  var config = {
+    revealTheme: '/reveal/theme-zenika/'
+  };
 
-  function applyPrintStylesheets() {
-    function addStylesheetWhenUrlMatches(regexp, stylesheet) {
-      if (!window.location.search.match(regexp)) return;
-      var linkElement = document.createElement('link');
-      linkElement.rel = 'stylesheet';
-      linkElement.type = 'text/css';
-      linkElement.href = stylesheet;
-      document.getElementsByTagName('head')[0].appendChild(linkElement);
-    }
-    addStylesheetWhenUrlMatches(/print-pdf/gi, '/node_modules/reveal.js/css/print/pdf.css');
-    addStylesheetWhenUrlMatches(/print-pdf/gi, '/reveal/theme-zenika/pdf.css');
-    addStylesheetWhenUrlMatches(/edition/gi, '/reveal/theme-zenika/edition.css');
-  }
+  isFileExist('reveal.js/css/reveal.min.css')
+    .then(function () {
+      // NPM 3
+      config.revealModule = 'reveal.js/';
+    })
+    .catch(function () {
+      // NPM 2
+      config.revealModule = 'node_modules/reveal.js/';
+    });
 
-  function insertSlides(callback) {
-    var req = new XMLHttpRequest();
-    req.onload = function () {
-      var slideContainer = document.querySelector('.slides');
-      JSON.parse(req.responseText).forEach(function (path) {
-        var chapter = document.createElement('section');
-        chapter.dataset.markdown = path;
-        chapter.dataset.vertical = '^\r?\n\r?\n\r?\n';
-        chapter.dataset.notes = '^Notes :';
-        slideContainer.appendChild(chapter);
-      });
-      callback();
-    };
-    req.open('GET', 'slides.json');
-    req.send();
-  }
+  Promise.all([
+    applyPrintStylesheets(),
+    insertSlides()
+  ]).then(runReveal);
 
   function runReveal() {
     // Full list of configuration options available here:
@@ -56,10 +39,10 @@
 
       // Optional libraries used to extend on reveal.js
       dependencies: [
-        { src: 'node_modules/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: 'node_modules/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'node_modules/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() {
+        { src: config.revealModule + 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: config.revealModule + 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: config.revealModule + 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: config.revealModule + 'plugin/highlight/highlight.js', async: true, callback: function() {
             // Define Plain Text language for Console output
             hljs.LANGUAGES.text = {
               keywords: '',
@@ -67,18 +50,89 @@
                 hljs.QUOTE_STRING_MODE
               ]
             };
-            
+
             var allCodeTags = document.querySelectorAll( 'pre code' );
-            for(var i=0; i < allCodeTags.length; i++) {
-              hljs.highlightBlock(allCodeTags[i]);
-            }
+            Array.prototype.forEach.call(allCodeTags, function(codeTag) {
+               hljs.highlightBlock(codeTag);
+            });
           }
         },
-        { src: 'node_modules/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: config.revealModule + 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: config.revealModule + 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
         { src: 'reveal/plugins/zenika-footer/zenika-footer.js', condition: function() { return !!document.body.classList; } }
       ]
     });
   }
 
-}());
+
+  function appendStylesheetWhenUrlMatches(head, regexp, stylesheets) {
+    if (window.location.search.match(regexp)) {
+      stylesheets.forEach(function (stylesheet) {
+        var linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.type = 'text/css';
+        linkElement.href = stylesheet;
+        head.appendChild(linkElement);
+      });
+    }
+  }
+
+  function applyPrintStylesheets() {
+    return new Promise(function (resolve) {
+      var head = document.getElementsByTagName('head')[0];
+      appendStylesheetWhenUrlMatches(head, /print-pdf/gi, [config.revealModule + 'css/print/pdf.css', config.revealTheme + 'pdf.css']);
+      appendStylesheetWhenUrlMatches(head, /edition/gi, [config.revealTheme + 'edition.css']);
+      resolve();
+    });
+  }
+
+  function insertSlides() {
+    return new Promise(function (resolve, reject) {
+
+      var request = new XMLHttpRequest();
+      request.responseType = 'json';
+
+      request.onload = function () {
+        var slideContainer = document.querySelector('.slides');
+
+        request.response.forEach(function (path) {
+          var chapter = document.createElement('section');
+          chapter.dataset.markdown = path;
+          chapter.dataset.vertical = '^\r?\n\r?\n\r?\n';
+          chapter.dataset.notes = '^Notes :';
+          slideContainer.appendChild(chapter);
+        });
+        resolve(request.response);
+      };
+
+      request.onerror = function (event) {
+        console.error(event);
+        reject('Erreur chargement des slides.');
+      };
+
+      request.open('GET', 'slides.json');
+      request.send();
+    });
+  }
+
+  function isFileExist(url) {
+    return new Promise(function (resolve, reject) {
+      var request = new XMLHttpRequest();
+      request.onload = function () {
+        console.log('onload', request.status);
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject('File inacessible');
+        }
+      };
+      request.onerror = function (event) {
+        console.log(event);
+      };
+      request.open('GET', url);
+      request.send();
+    });
+  }
+
+
+})(window, document, Promise, Reveal);
