ac097d75ceb8c6b2aa121bccd1f67f66143c0006
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-10-06 08:35:11 +0200

    feat(gulp): ajout d'un dossier de build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var path = require('path');
+var del = require('del');
 var gulp = require('gulp');
 var g = require('gulp-load-plugins')();
 var browserSync = require('browser-sync').create();
@@ -16,26 +17,59 @@ var fwkConfig = {
   port: 8000,
   slidesPdfName: prefixPdfName + '-Slides',
   cahierExercicesPdfName: prefixPdfName + '-CahierExercices',
-  frameworkPath: __dirname
+  frameworkPath: __dirname,
+  outputPath: './build'
 };
 
 /**
- * Remplacement du placeholder par le nom de la formation
+ * Clean build file
  */
-gulp.task('templates', function () {
+gulp.task('clean', function () {
+  del([fwkConfig.outputPath]);
+});
 
-  return gulp.src([path.join(fwkConfig.frameworkPath, 'index_base.html')])
+/**
+ * Construction du dossier de build
+ */
+gulp.task('build', ['copybase', 'copyreveal'], function () {
+
+  // Remplacement du placeholder par le nom de la formation
+  return gulp.src([path.join(fwkConfig.frameworkPath, 'index.html')])
     .pipe(g.replace(/(FORMATION_NAME)/g, fwkConfig.slidesPdfName))
-    .pipe(gulp.dest(fwkConfig.frameworkPath));
+    .pipe(gulp.dest(fwkConfig.outputPath));
+});
+
+/**
+ * Copie du contenu web du framework dans le dossier de build
+ */
+gulp.task('copybase', ['clean'], function () {
+
+  return gulp.src(
+    [
+      path.join(fwkConfig.frameworkPath, 'index.html'),
+      path.join(fwkConfig.frameworkPath, '*.css'),
+      path.join(fwkConfig.frameworkPath, '!(node_modules)/**/*')
+    ])
+    .pipe(gulp.dest(fwkConfig.outputPath));
+});
+
+/**
+ * Copie du contenu le Reveal.js dans le dossier de build
+ */
+gulp.task('copyreveal', ['clean'], function () {
+
+  return gulp.src('./node_modules/reveal.js/!(node_modules)/**/*')
+    .pipe(gulp.dest('./build/reveal.js'));
 });
 
 /**
  * Serve public folder and watch all changes
  */
-gulp.task('serve', ['templates'], function () {
+gulp.task('serve', ['build'], function () {
   browserSync.init({
+    port: fwkConfig.port,
     server: {
-      baseDir: [fwkConfig.frameworkPath, './node_modules', './Slides']
+      baseDir: [fwkConfig.outputPath, './Slides']
     }
   });
 
@@ -43,9 +77,16 @@ gulp.task('serve', ['templates'], function () {
               'Slides/**/*.md',
               'Slides/slides.json',
               'Slides/ressources/**',
-              path.join(fwkConfig.frameworkPath, 'reveal/**'),
-              path.join(fwkConfig.frameworkPath, 'index.html')
+              path.join(fwkConfig.outputPath, 'reveal/**'),
+              path.join(fwkConfig.outputPath, 'index.html')
             ], browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
+
+// TODO
+// package
+// generateSlidesPDF
+// generateCahierExercice
+// 'pdf' => ['generateSlidesPDF', 'generateCahierExercice']
+
