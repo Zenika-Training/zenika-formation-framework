d5641af0f43195b1097433c595473b9f3de257b5
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-10-18 18:06:18 +0200

    fix gae deploy attempt #1

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,4 +1,5 @@
 echo $GAE_KEY_FILE_CONTENT > file.key
+google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to FORMATION_DEPLOY_NAME"
