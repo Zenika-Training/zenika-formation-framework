22fca023ab659bd561b44d0d9144e87a25371c81
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-12-22 11:20:40 +0100

    fix gcloud version to prevent beta changes

--- gcloud-install.sh
+++ gcloud-install.sh
@@ -1,7 +1,7 @@
-if [[ ! -f google-cloud-sdk/path.bash.inc ]]; then
-  echo "Downloading google cloud sdk";
-  wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip;
-  unzip -qq google-cloud-sdk.zip;
-  google-cloud-sdk/install.sh --usage-reporting false --path-update false --rc-path=~/.bashrc --bash-completion false;
-fi;
+echo "Downloading google cloud sdk";
+wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip;
+unzip -qq google-cloud-sdk.zip;
+export CLOUDSDK_COMPONENT_MANAGER_FIXED_SDK_VERSION=0.9.86;
+google-cloud-sdk/install.sh --usage-reporting false --path-update false --rc-path=~/.bashrc --bash-completion false;
+google-cloud-sdk/bin/gcloud config set --scope=installation component_manager/fixed_sdk_version $CLOUDSDK_COMPONENT_MANAGER_FIXED_SDK_VERSION;
 google-cloud-sdk/bin/gcloud components update --quiet;
