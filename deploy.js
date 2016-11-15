65ec4fc69dfee36f0f97d2825a41c9e357fd5677
Hugo Wood <hx4.5@free.fr>
2016-11-15 14:31:35 +0100

    chore(#130): deploy bin script (#131)
    * chore(#130): js equivalent of gcloud-deploy.sh to make it available for dependents as a bin script
    
    * chore(#130): suppression des scripts gcloud
    
    * feat: updated script for more recent version of gcloud sdk (see #104)

---
+++ deploy.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const execFileSync = require('child_process').execFileSync
+const fs = require('fs')
+
+const tempKeyFile = 'file.key'
+const serviceAccount = process.env.GAE_SERVICE_ACCOUNT
+const deployName = process.env.npm_package_config_deploy_name
+const currentBranch = process.env.CURRENT_BRANCH
+
+if (!serviceAccount || !currentBranch) {
+  console.log('Environment variables GAE_SERVICE_ACCOUNT or CURRENT_BRANCH not set.')
+  console.log('Please check both are defined when running this script.')
+  process.exit(1)
+}
+
+fs.writeFileSync(tempKeyFile, process.env.GAE_KEY_FILE_CONTENT)
+try {
+  execFileSync('gcloud', ['config', 'set', 'app/use_appengine_api', 'false'])
+  execFileSync('gcloud', ['config', 'set', 'app/promote_by_default', 'false'])
+  console.log('Authenticate with', serviceAccount)
+  execFileSync('gcloud', ['auth', 'activate-service-account', serviceAccount, '--key-file', tempKeyFile])
+  console.log('Deploying to', deployName)
+  execFileSync('gcloud', ['--project', deployName, 'preview', 'app', 'deploy', '--version', currentBranch, '--quiet', 'dist/app.yaml'])
+} finally {
+  fs.unlinkSync(tempKeyFile)
+}
--- gcloud-deploy.sh
+++
@@ -1,6 +0,0 @@
-echo $GAE_KEY_FILE_CONTENT > file.key
-google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
-echo "Authenticate with $GAE_SERVICE_ACCOUNT"
-google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
-echo "Deploying to FORMATION_DEPLOY_NAME"
-google-cloud-sdk/bin/gcloud --project FORMATION_DEPLOY_NAME preview app deploy --version $CURRENT_BRANCH --quiet dist/app.yaml
