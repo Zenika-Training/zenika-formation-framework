29814db7d8799bf417378c9a222a225c2d30cbf3
Hugo Wood <hugo.wood-ext@vif.fr>
2017-03-01 16:11:37 +0100

    chore(phantomjs): upgrade to phantomjs 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -346,7 +346,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('doGenerateSlidesPDF', function () {
     var childProcess = require('child_process');
-    var phantomjs = require('phantomjs');
+    var phantomjs = require('phantomjs-prebuilt');
     var binPath = phantomjs.path;
     var done = grunt.task.current.async();
 
