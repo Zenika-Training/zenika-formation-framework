3060620b3e9b588d602e4963b3c3f7340a1752e8
Hugo Wood <hx4.5@free.fr>
2017-05-03 12:30:49 +0200

    style: eslint with airbnb config (#183)
    * style: eslint with airbnb config
    
    4 rules were disabled to tweaked because they were too complex to comply with right now. They should be enabled in the future.
    
    * chore(git): lint pre-commit hook
    
    * fix(gruntfile): syntax error
    
    Node 7 does not support trailing commas in argument lists

--- ic.js
+++ ic.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
-if (require.main === module) { // or else the Gruntfile from the depending project will run the code!
-  const assert = require('assert');
-  const fs = require('fs');
+// or else the Gruntfile from the depending project will run the code!
+if (require.main === module) {
   const request = require('request-promise-native');
   const username = process.env.CIRCLE_TOKEN;
   const password = '';
-  const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+  const auth = `Basic ${new Buffer(`${username}:${password}`).toString('base64')}`;
 
-  const projectName = process.argv[2] || require('./package.json').name
+  const projectName = process.argv[2] || require('./package.json').name;
 
   const firstCheckToken = {
     method: 'GET',
     uri: 'https://circleci.com/api/v1.1/me',
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     json: true,
     resolveWithFullResponse: false,
@@ -24,7 +23,7 @@ if (require.main === module) { // or else the Gruntfile from the depending proje
     method: 'POST',
     uri: `https://circleci.com/api/v1/project/Zenika/${projectName}/follow`,
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     json: true,
   };
@@ -33,7 +32,7 @@ if (require.main === module) { // or else the Gruntfile from the depending proje
     method: 'POST',
     uri: `https://circleci.com/api/v1/project/Zenika/${projectName}/envvar`,
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     body: {
       name: 'GAE_SERVICE_ACCOUNT',
@@ -46,7 +45,7 @@ if (require.main === module) { // or else the Gruntfile from the depending proje
     method: 'POST',
     uri: `https://circleci.com/api/v1/project/Zenika/${projectName}/envvar`,
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     body: {
       name: 'GAE_KEY_FILE_CONTENT',
@@ -59,34 +58,34 @@ if (require.main === module) { // or else the Gruntfile from the depending proje
     method: 'DELETE',
     uri: `https://circleci.com/api/v1/project/Zenika/${projectName}/build-cache`,
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     json: true,
-  }
+  };
 
   const retryMasterBuild = () => ({
     method: 'POST',
     uri: `https://circleci.com/api/v1/project/Zenika/${projectName}/tree/master`,
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     json: true,
-  })
+  });
 
   request(firstCheckToken)
     .then(data => console.log(`👷 Welcome ${data.login}`))
     .then(() => request(secondCreateProject))
-    .then(data => {
-      if(!data.first_build) return console.log(`🚧 Project ${projectName} already exists`)
-      return console.log(`🚧 Project ${projectName} created`)
+    .then((data) => {
+      if (!data.first_build) return console.log(`🚧 Project ${projectName} already exists`);
+      return console.log(`🚧 Project ${projectName} created`);
     })
     .then(() => request(thirdSetEnvVariableGaeServiceAccount))
     .then(() => request(thirdSetEnvVariableGaeKey))
-    .then(() => console.log(`🔧 Env variables set!`))
-    .then(() => console.log(`💣 Clearing cache...`))
+    .then(() => console.log('🔧 Env variables set!'))
+    .then(() => console.log('💣 Clearing cache...'))
     .then(() => request(deleteCache))
-    .then(() => { console.log(`✨ Re-building master`) })
+    .then(() => { console.log('✨ Re-building master'); })
     .then(() => request(retryMasterBuild()))
-    .then(() => console.log(`💚 All is done! Wait for a green deployment`))
-    .catch(err => console.log('💩 AieAieAie!\n', err))
+    .then(() => console.log('💚 All is done! Wait for a green deployment'))
+    .catch(err => console.log('💩 AieAieAie!\n', err));
 }
--- rebuild-all.js
+++ rebuild-all.js
@@ -1,46 +1,35 @@
 #!/usr/bin/env node
 
-if (require.main === module) { // or else the Gruntfile from the depending project will run the code!
-  const childProcess = require('child_process')
+// or else the Gruntfile from the depending project will run the code!
+if (require.main === module) {
+  const childProcess = require('child_process');
   const request = require('request-promise-native');
   const username = process.env.CIRCLE_TOKEN;
   const password = '';
-  const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-
-  const projectName = process.argv[2] || require('./package.json').name
-
-  const firstCheckToken = {
-    method: 'GET',
-    uri: 'https://circleci.com/api/v1.1/me',
-    headers: {
-      'Authorization': auth
-    },
-    json: true,
-    resolveWithFullResponse: false,
-  }
+  const auth = `Basic ${new Buffer(`${username}:${password}`).toString('base64')}`;
 
   const getProjects = {
     method: 'GET',
-    uri: `https://circleci.com/api/v1/projects`,
+    uri: 'https://circleci.com/api/v1/projects',
     headers: {
-      'Authorization': auth
+      Authorization: auth,
     },
     json: true,
-  }
+  };
 
   request(getProjects)
-    .then(projects => {
+    .then((projects) => {
       const projectsToRebuild = projects
-        .map(project => {
-          const urlSegments = project.vcs_url.split('/')
-          return urlSegments[urlSegments.length - 1]
+        .map((project) => {
+          const urlSegments = project.vcs_url.split('/');
+          return urlSegments[urlSegments.length - 1];
         })
-        .filter(project => project.startsWith('formation-'))
-      console.log(`rebuilding ${projectsToRebuild.length} projects`)
-      projectsToRebuild.forEach(project => {
-        console.log(project)
-        childProcess.spawnSync('node', ['ic.js', project])
-      })
+        .filter(project => project.startsWith('formation-'));
+      console.log(`rebuilding ${projectsToRebuild.length} projects`);
+      projectsToRebuild.forEach((project) => {
+        console.log(project);
+        childProcess.spawnSync('node', ['ic.js', project]);
+      });
     })
-    .catch(err => console.log('💩 AieAieAie!\n', err))
+    .catch(err => console.log('💩 AieAieAie!\n', err));
 }
