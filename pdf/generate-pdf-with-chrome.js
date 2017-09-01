045756a7c38ae44d837fcd4d8c99e281f42d7b44
Hugo Wood <hx4.5@free.fr>
2017-09-01 17:19:40 +0200

    :hammer: labs pdf generation with Chrome (#202)

--- pdf/generate-pdf-with-chrome.js
+++ pdf/generate-pdf-with-chrome.js
@@ -1,11 +1,20 @@
 const puppeteer = require('puppeteer');
 
+async function generatePdfFromHtml(html, options) {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const page = await browser.newPage();
+  await page.setContent(html);
+  const pdfContent = await page.pdf(options);
+  browser.close();
+  return pdfContent.toString('base64');
+}
+
 /**
  * @param {string} url URL where the slides are served
  * @param {object} options see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
  * @returns {Promise<string>} PDF content in base64
  */
-async function generatePdf(url, options) {
+async function generatePdfAt(url, options) {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto(url);
@@ -18,4 +27,4 @@ async function generatePdf(url, options) {
   return pdfContent.toString('base64');
 }
 
-module.exports = generatePdf;
+module.exports = { generatePdfAt, generatePdfFromHtml };
