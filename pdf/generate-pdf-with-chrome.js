b7d752b2ed30c1a23210dd19dd5e156e0b473b26
Hugo Wood <hx4.5@free.fr>
2017-09-01 11:16:50 +0200

    :hammer: use puppeteer for Chrome control
    This will enable us to have simple nodejs Docker image to generate PDFs since puppeteer will install Chrome inside it, instead of having to have a Chrome Docker image.

--- pdf/generate-pdf-with-chrome.js
+++ pdf/generate-pdf-with-chrome.js
@@ -1,74 +1,21 @@
-/**
- * This module exposes a function that runs Chrome, connects to it via the
- * debug protocol, then navigates to the given URL where the slides should be 
- * running, waits for Reveal to be ready then generates and returns a PDF as a
- * base 64 string.
- *
- * Which Chrome to run is auto-detected using the Chrome Launcher from the
- * Lighthouse project. Chrome is run headless if available. Chrome Canary
- * is run if available.
- *
- * Resources:
- * Getting started with Chrome Headless
- * https://developers.google.com/web/updates/2017/04/headless-chrome
- * Chrome Remote Interface Documention
- * https://chromedevtools.github.io/devtools-protocol/
- */
-
-const { ChromeLauncher } = require('lighthouse/lighthouse-cli/chrome-launcher');
-const chromeRemoteInterface = require('chrome-remote-interface');
-
-async function launchChrome() {
-  const launcher = new ChromeLauncher({
-    port: 9222,
-    autoSelectChrome: true,
-    additionalFlags: [
-      '--disable-gpu',
-      '--headless',
-    ],
-  });
-
-  try {
-    await launcher.run();
-  } catch (err) {
-    await launcher.kill();
-    throw err;
-  }
-
-  return launcher;
-}
-
-function waitForReveal(Runtime) {
-  return new Promise((resolve) => {
-    const timer = setInterval(async () => {
-      const { result } = await Runtime.evaluate({
-        expression: 'window.Reveal.isReady()',
-      });
-      if (result.value) {
-        clearInterval(timer);
-        resolve();
-      }
-    }, 500);
-  });
-}
+const puppeteer = require('puppeteer');
 
 /**
  * @param {string} url URL where the slides are served
+ * @param {object} options see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
  * @returns {Promise<string>} PDF content in base64
  */
 async function generatePdf(url, options) {
-  const launcher = await launchChrome();
-  const protocol = await chromeRemoteInterface();
-  const { Page, Runtime } = protocol;
-  await Page.enable();
-  await Runtime.enable();
-  await Page.navigate({ url });
-  await Page.loadEventFired();
-  await waitForReveal(Runtime);
-  const pdf = await Page.printToPDF(options);
-  protocol.close();
-  launcher.kill();
-  return pdf.data;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForFunction('window.Reveal.isReady()', {
+    polling: 500,
+    timeout: 10000,
+  });
+  const pdfContent = await page.pdf(options);
+  browser.close();
+  return pdfContent.toString('base64');
 }
 
 module.exports = generatePdf;
